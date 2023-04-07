0a7ffaf524beacd1a89deba8e881621713e35226
Matt Grieser <mattgrieser@users.noreply.github.com>
2023-04-07 13:57:21 -0400

    Improvements for readability and maintainability
    Removed today and yesterday parameters from the download_pdf() function. Instead, the function now returns a boolean value indicating whether the download was successful or not.
    Moved the logic for handling the WSJ case (when the response status code is 404) outside the download_pdf() function and into the download_pdfs() function.
    Combined the URLs into a single list with just the PDF names and created a base_url variable for better maintainability.
    Iterated over the list of PDF names instead of URLs, making it easier to create the formatted URL and filename for each PDF.

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,64 +1,49 @@
-# Import required libraries
 import os
 import datetime
 import requests
 
-# Function to download a PDF from a URL and save it to a specified filepath
-def download_pdf(url, filepath, today, yesterday):
-    # Send a GET request to the URL
+def download_pdf(url, filepath):
     response = requests.get(url)
 
-    # If the response is a 404 error and the URL is for WSJ.pdf,
-    # try downloading the PDF with yesterday's date in the URL
-    if response.status_code == 404 and 'WSJ.pdf' in url:
-        # Replace today's day with yesterday's day in the URL
-        url_yesterday = url.replace(str(today.day), str(yesterday.day))
-        # Send a GET request to the updated URL
-        response = requests.get(url_yesterday)
-
-    # If the response status code is 200 (successful), write the content to the specified filepath
     if response.status_code == 200:
         with open(filepath, 'wb') as f:
             f.write(response.content)
-    # Otherwise, print an error message
     else:
         print(f"Failed to download {url}: {response.status_code}")
+        return False
 
-# Function to download PDFs from the specified URLs
+    return True
+
 def download_pdfs():
-    # Get the current date and time
     current_date = datetime.datetime.now()
-    # Get yesterday's date by subtracting one day from the current date
     yesterday = current_date - datetime.timedelta(days=1)
 
-    # List of URLs to download PDFs from, with the day of the month inserted into the URL
     urls = [
-        f"https://cdn.freedomforum.org/dfp/pdf{current_date.day}/IL_CT.pdf",
-        f"https://cdn.freedomforum.org/dfp/pdf{current_date.day}/IN_IS.pdf",
-        f"https://cdn.freedomforum.org/dfp/pdf{current_date.day}/CA_LAT.pdf",
-        f"https://cdn.freedomforum.org/dfp/pdf{current_date.day}/NY_NYT.pdf",
-        f"https://cdn.freedomforum.org/dfp/pdf{current_date.day}/WSJ.pdf",
+        "IL_CT.pdf",
+        "IN_IS.pdf",
+        "CA_LAT.pdf",
+        "NY_NYT.pdf",
+        "WSJ.pdf",
     ]
 
-    # Define the download directory
+    base_url = "https://cdn.freedomforum.org/dfp/pdf"
     download_directory = os.path.expanduser("~/Library/Mobile Documents/com~apple~CloudDocs/@0 Inbox/FrontPages")
 
-    # If the download directory does not exist, create it
     if not os.path.exists(download_directory):
         os.makedirs(download_directory)
 
-    # Format the current date as a string in the format "YYYY-MM-DD"
     formatted_date = current_date.strftime("%Y-%m-%d")
 
-    # Iterate through the list of URLs
-    for url in urls:
-        # Generate a filename with the current date and the PDF's name
-        filename = f"{formatted_date}_{url.split('/')[-1].split('.')[0]}.pdf"
-        # Generate the full filepath by combining the download directory and filename
+    for pdf_name in urls:
+        formatted_url = f"{base_url}{current_date.day}/{pdf_name}"
+        filename = f"{formatted_date}_{pdf_name.split('.')[0]}.pdf"
         filepath = os.path.join(download_directory, filename)
-        # Call the download_pdf function with the URL, filepath, current date, and yesterday's date
-        download_pdf(url, filepath, current_date, yesterday)
 
-# If this script is being run as the main module, call the download_pdfs function
+        success = download_pdf(formatted_url, filepath)
+
+        if not success and 'WSJ.pdf' in pdf_name:
+            formatted_url_yesterday = f"{base_url}{yesterday.day}/{pdf_name}"
+            download_pdf(formatted_url_yesterday, filepath)
+
 if __name__ == "__main__":
     download_pdfs()
